afde4b0165d66ee54409dbcd7ae61f912b57a9f1
kzc <792187001@qq.com>
2021-05-17 14:06:54 +0800

    abc

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/TrademarkController.java
@@ -0,0 +1,30 @@
+package com.atguigu.gmall.product.controller;
+import com.atguigu.gmall.common.util.Result;
+import com.atguigu.gmall.model.product.BaseTrademark;
+import com.atguigu.gmall.product.service.BaseTrademarkService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("admin/product/")
+@CrossOrigin
+public class TrademarkController {
+
+    @Autowired
+    BaseTrademarkService baseTrademarkService;
+
+    @RequestMapping("baseTrademark/{page}/{limit}")
+    public Result baseTrademark(@PathVariable("page") Long page,@PathVariable("limit") Long limit){
+
+        IPage<BaseTrademark> baseTrademarks=baseTrademarkService.baseTrademark(page,limit);
+
+        return Result.ok(baseTrademarks);
+    }
+
+}
